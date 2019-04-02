f4393b6499bae3dd53a2171bc85c31a06c2613b2
XuchunS <movikeeper.cg@outlook.com>
2019-04-02 17:20:12 +0800

    sliding window

--- lengthOfLongestSubstring.cpp
+++ lengthOfLongestSubstring.cpp
@@ -3,25 +3,26 @@ public:
     int lengthOfLongestSubstring(string s) 
     {
         int sLen = s.length();
-        int len = 0;
         string ans = "";
         int val = 0;
-        for(int i = 0;i < sLen;i++)
-            for(int j = i;j < sLen;j++)
+        int j = 0;
+        int len = 0;
+            
+        while(j < sLen)
+        {
+            if(ans.find(s[j]) == string::npos)
             {
-                if(ans.find(s[j]) == string::npos) 
-                {
-                    ans = ans + s[j];
-                    len = ans.length();
-                    if(len > val)
-                        val = len;
-                }
-                else
-                {
-                    ans = "";
-                    break;
-                }
+                ans = ans + s[j];
+                j++;
+                len = ans.length();
+                if(len > val)
+                    val = len;
             }
+            else
+            {
+                ans = ans.substr(1);
+            }
+        }
         return val;
     }
 };
